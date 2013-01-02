eb61713b45c6288429473c587ec0beb72d6bf135
Griffin Boyce <griffin@cryptic.be>
2013-01-02 11:41:44 -0500

    For the supervillulz

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ wp-etiquette
 ============
 
 Displays guidelines above comment area. Etiquette, eh wot?
+
+For the supervillains who enjoy the finer things in life.
+
+**License**: MIT, a supervillain-friendly open license
---
+++ etiquette.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    Plugin Name: WP-Etiquette
+    Plugin URI: http://github.com/glamrock/wp-etiquette
+    Description: Displays guidelines above comment area. Etiquette, eh wot?
+    Version: 0.1
+    Author: Griffin Boyce
+    Author URI: http://cryptic.be
+    License: MIT
+*/
+?>
